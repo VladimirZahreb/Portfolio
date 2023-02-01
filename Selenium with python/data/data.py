3c6a80d4c90f55cfbac61fbdabc3216b0dc26817
VladimirZahreb <106901086+VladimirZahreb@users.noreply.github.com>
2023-02-01 15:28:14 +0200

    Update data.py

--- Selenium with python/data/data.py
+++ Selenium with python/data/data.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-"""Generating of the random test data to create the person"""
 @dataclass
 class Person:
     full_name: str = None
@@ -14,12 +13,10 @@ class Person:
     permanent_address: str = None
     mobile: str = None
         
-"""Random of the color generation"""
 @dataclass
 class Color:
     color_name: list = None
 
-"""Random of the date generation"""
 @dataclass
 class Date:
     day: str = None
